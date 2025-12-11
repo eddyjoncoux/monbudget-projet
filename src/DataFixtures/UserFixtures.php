2d0f00b978f5f9fa6693d14a4163b26817061bce
Eddy <125514698+KarlRow@users.noreply.github.com>
2025-12-11 21:50:38 +0100

    Ajout d'un user dans la bdd

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setUsername('eddydevwebb');
+        $user->setPassword(1234);
+        $user->setRoles(['ROLES_USER']);
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
