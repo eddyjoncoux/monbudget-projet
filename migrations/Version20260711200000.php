3bcc6eb649c4200b4bac344a9a2391842436595d
Eddy <125514698+KarlRow@users.noreply.github.com>
2026-07-11 16:55:23 +0200

    Fix: Rename migration to correct execution order

--- migrations/Version20260711200000.php
+++ migrations/Version20260711200000.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20260711112000 extends AbstractMigration
+final class Version20260711200000 extends AbstractMigration
 {
     public function getDescription(): string
     {
